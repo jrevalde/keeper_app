f6432f8250ae9dfa63e33d65931d1c4f86638fa2
jrevalde <jethro.revalde@hotmail.com>
2022-05-20 19:31:47 +1000

    not taking app using react.js

---
+++ build/precache-manifest.d9caab342a198d44b419cbda2010ef8a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f5b73894620ce0b6bc74d6a45ccfd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "491225e4f0ff0f3494f7",
+    "url": "/static/js/main.b807485f.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
